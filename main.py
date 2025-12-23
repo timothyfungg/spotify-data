03805f370d94ff9fefa96c43cd6232b54f4b7c2f
timothyfungg <timothyfung22@gmail.com>
2025-12-23 18:13:04 -0500

    Update main
    Created the logic for adding and updating song data to songs

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-class Main:
-    def main(filename):
-        songs = [] # 2d list that stores song data in the format of [name, artist, total time played, total number of plays]
-        file = open(filename, "r")
-        for line in file:
-            # store:
-            time = # ms_played
-            name = # master_metadata_track_name
-            artist = # master_metadata_album_artist_name
-            skipped = # skipped (for number of plays)
-            
-            # add song to list of not already there
-            # update data of corresponding song in list
+songs = [] # 2d list that stores song data in the format of [name, artist, total time played, total number of plays]
+def main(filename):
+    file = open(filename, "r")
+    for line in file:
+        # store:
+        time = # ms_played
+        name = # master_metadata_track_name
+        artist = # master_metadata_album_artist_name
+        skipped = # skipped (for number of plays)
+        
+        # Boolean to keep track of whether the song is in the list or not
+        present = False
+        
+        # Checks entire list to check if song is present, updates data if so
+        for i in range(0, len(songs)):
+            if(songs[i][0] == name):
+                check_song(i, time, skipped)
+                present = True
+        
+        # Adds song to list if not present and updates its data
+        if(present == False)
+            songs.append([name, artist, 0, 0])
+            check_song(len(songs - 1), time, skipped)
 
-    def check_song(songs, index, time, skipped):
-        songs[index][2] += time
-        if(skipped == True):
-            songs[index][3] += 1
-        return songs
+def check_song(index, time, skipped):
+    songs[index][2] += time
+    if(skipped == True):
+        songs[index][3] += 1
 
 """ Data template
 {
