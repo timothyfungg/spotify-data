5fb284bc85717a4cf78fe3fdff7524a9078a7a70
timothyfungg <timothyfung22@gmail.com>
2025-12-24 16:06:47 -0500

    Update main
    Created code for deciding which lines to parse data to be put into manage_song

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 songs = [] # 2d list that stores song data in the format of [name, artist, total time played, total number of plays]
 def main(filename):
     file = open(filename, "r")
-    for line in file:
-        # store:
+    # store:
         time = # ms_played
         name = # master_metadata_track_name
         artist = # master_metadata_album_artist_name
         skipped = # skipped (for number of plays)
+        i = 1
+    for line in file:
+        # update song name
+        if(((i - 6) % 23) == 0): # first time on 6th line, next 23 lines after
+            time = int(line[17:].strip(,))
+        elif(((i - 10) % 23) == 0): # first name on 10th line, next 23 lines after
+            name = line[34:].strip(,)
+        elif(((i - 11) % 23) == 0): # first artist on 11th line, next 23 lines after
+            artist = line[37:].strip(,)
+        elif(((i - 20) % 23) == 0): # first skipped on 20th line, next 23 lines after
+            skipped = line[15:].strip(,)
+            if(skipped == "true"):
+                skipped = True
+            else:
+                skipped = False
+            manage_song(time, name, artist, skipped)
+        i += 1
         
-        # Boolean to keep track of whether the song is in the list or not
-        present = False
-        
-        # Checks entire list to check if song is present, updates data if so
-        for i in range(0, len(songs)):
-            if(songs[i][0] == name):
-                check_song(i, time, skipped)
-                present = True
-        
-        # Adds song to list if not present and updates its data
-        if(present == False)
-            songs.append([name, artist, 0, 0])
-            check_song(len(songs - 1), time, skipped)
 
 def check_song(index, time, skipped):
     songs[index][2] += time
     if(skipped == True):
         songs[index][3] += 1
+
+def manage_song(time, name, artist, skipped):
+    # Boolean to keep track of whether the song is in the list or not
+    present = False
+    
+    # Checks entire list to check if song is present, updates data if so
+    for i in range(0, len(songs)):
+        if(songs[i][0] == name):
+            check_song(i, time, skipped)
+            present = True
+    
+    # Adds song to list if not present and updates its data
+    if(present == False)
+        songs.append([name, artist, 0, 0])
+        check_song(len(songs - 1), time, skipped)
 
 """ Data template
 {
